d96a2e849ac0a9315c1fa966898b1f8b1fe5be7e
Ashlin Jones <ajones@crowdrise.com>
2016-08-15 18:29:39 -0700

    Added much much needed comments

--- tick-tock.py
+++ tick-tock.py
@@ -1,8 +1,10 @@
 import time
 from termcolor import colored
 
+#OCD dicates the screen be cleared on first run
 print(chr(27) + "[2J")
 
+#always follow the six aristotelian elements of play
 print '\n'.join([
   'Welcome to the clock-hands program.',
   'Give it the time in integer format',
@@ -12,15 +14,20 @@ print '\n'.join([
   'and your head might explode!','\n\n\n'
 ])
 
-def magic_time(hour, minute):
-    degrees = abs((hour * 30 + minute * 0.5) - (minute * 6))
+#main function that takes two arguments
+def magic_time(hourhand, minutehand):
+    #formulate the distance in between clock hands by reducing and calculating the difference
+    degrees = abs((hourhand * 30 + minutehand * 0.5) - (minutehand * 6))
     print(degrees) 
 
-hour   = int( input("Input the hour: ") )
-minute = int( input("Input the minute: ") )
+#necessary variables
+hourhand   = int( input("Input the hour: ") )
+minutehand = int( input("Input the minute: ") )
 
+#more elements of play...
 print colored('Oh, I usually have tea at that time!', 'yellow')
 print colored('The degrees between your clock hands equals... ', 'red')
-time.sleep(5)
- 
-magic_time(hour, minute)
+time.sleep(3)
+
+#result 
+magic_time(hourhand, minutehand)
